f0f40ed39033ce4d67ddb26b3d9613330e58c5d9
marotella <michelle.a.rotella@gmail.com>
2024-01-18 21:51:36 -0500

    controllers file

---
+++ controllers.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "getHouses": (req, res)=>{
+
+    },
+    "deleteHouse": (req, res) =>{
+
+    },
+    "createHouse": (req, res) =>{
+
+    },
+    "updateHouse": (req, res)=>{
+        
+    }
+
+}    
