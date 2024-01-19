6cf767d5b3e9a2201975ce3335bb270330cbaeec
marotella <michelle.a.rotella@gmail.com>
2024-01-18 21:59:42 -0500

    created end points

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express ()
 app.use(express.json());
 app.use(cors());
 
-
+app.get("/api/getAll", ctrl.getHouses)
+app.post("/api/newHouse", ctrl.createHouse)
+app.put("/api/updateHouse/:id", ctrl.updateHouse)
+app.delete("/api/deleteHouse/:id", ctrl.deleteHouse)
 
 app.listen(4000, () => console.log("Server running on port 4000"))
