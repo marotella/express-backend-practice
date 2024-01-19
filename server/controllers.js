38cb4fcae03a0fbfa94429fc9b1a682b2747b43d
marotella <michelle.a.rotella@gmail.com>
2024-01-18 22:15:52 -0500

    delete functionality working

--- server/controllers.js
+++ server/controllers.js
@@ -6,7 +6,10 @@ module.exports = {
         res.status(200).send(houseDB)
     },
     "deleteHouse": (req, res) =>{
-
+        const {id} = req.params;
+        const index = houseDB.findIndex(element => +element.id === +id)
+        houseDB.splice(index, 1);
+        res.status(200).send(houseDB)
     },
     "createHouse": (req, res) =>{
 
