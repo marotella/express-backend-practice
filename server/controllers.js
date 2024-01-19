2ccc2d54132b4279140f5658ee145561a914f7e8
marotella <michelle.a.rotella@gmail.com>
2024-01-18 22:28:18 -0500

    update functionality completed

--- server/controllers.js
+++ server/controllers.js
@@ -2,17 +2,17 @@
 let globalID = 4;
 const houseDB = require("./db.json")
 module.exports = {
-    "getHouses": (req, res)=>{
+    "getHouses": (req, res) => {
         res.status(200).send(houseDB)
     },
-    "deleteHouse": (req, res) =>{
-        const {id} = req.params;
+    "deleteHouse": (req, res) => {
+        const { id } = req.params;
         const index = houseDB.findIndex(element => +element.id === +id)
         houseDB.splice(index, 1);
         res.status(200).send(houseDB)
     },
-    "createHouse": (req, res) =>{
-        const {address, price, imageURL} = req.body
+    "createHouse": (req, res) => {
+        const { address, price, imageURL } = req.body
         const id = globalID
         houseDB.push({
             id,
@@ -23,8 +23,21 @@ module.exports = {
         res.status(200).send(houseDB)
         globalID++
     },
-    "updateHouse": (req, res)=>{
+    "updateHouse": (req, res) => {
+        const { id } = req.params;
+        const { type } = req.body;
+        const index = houseDB.findIndex(element => +element.id === +id)
+        if (type === "plus") {
+            houseDB[index].price = houseDB[index].price + 10000
+        }
+        if (type === "minus") {
+            houseDB[index].price = houseDB[index].price - 10000
+        }
+        res.status(200).send(houseDB)
 
     }
+        
 
-}    
+}
+
+
